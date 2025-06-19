75c7393f63ae4c476dd2ba65c3505a73a82d0c8c
natancelidonio <natan.c.f.adm@gmail.com>
2025-06-19 18:33:45 -0300

    Relações entrer classes: associação, agregação e composição

---
+++ aula215.py
@@ -0,0 +1,28 @@
+class Escritor:
+		def __init__(self, nome) -> None:
+				self.nome = nome
+				self._ferramenta = None
+		
+		@property
+		def ferramenta(self):
+				return self._ferramenta
+		
+		@ferramenta.setter
+		def ferramenta(self, ferramenta):
+				self._ferramenta = ferramenta
+
+class FerramentaDeEscrever:
+		def __init__(self, nome):
+				self.nome = nome
+		
+		def escrever(self):
+				return f'{self.nome} está escrevendo'
+
+escritor = Escritor('Luiz')
+caneta = FerramentaDeEscrever('Caneta Bic')
+maquina_de_escrever = FerramentaDeEscrever('Máquina')
+escritor.ferramenta = maquina_de_escrever
+
+print(caneta.escrever())    # Caneta Bic está escrevendo
+print(maquina_de_escrever.escrever())    # Máquina está escrevendo
+print(escritor.ferramenta.escrever())    # Máquina está escrevendo
