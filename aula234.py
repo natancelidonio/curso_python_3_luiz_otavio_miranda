38ebdcaa5a179ad015f3ba1fa4435d6824a14a12
natancelidonio <natan.c.f.adm@gmail.com>
2025-06-25 15:58:44 -0300

    Criando, levantando e tratando Exceptions; e Notas das exceptions em Python 3.11+

---
+++ aula234.py
@@ -0,0 +1,17 @@
+class MeuError(Exception):
+	...
+
+class OutroError(Exception):
+    ...
+def levantar():
+    exception_ = MeuError('a', 'b', 'c')
+    raise exception_
+
+try:
+    levantar()
+except MeuError as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    print()
+    exception_ = OutroError('Vou lançar de novo')
+    raise  exception_ from error
